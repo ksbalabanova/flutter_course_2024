3bac36893b25f6bdfc9b198775c8c2e27bf75f37
ksbalabanova <kseniabalabanova130@gmail.ru>
2024-12-06 20:16:56 +0300

    prac4

--- practice_4/lib/main.dart
+++ practice_4/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:practice_4/presentation/counter_flow/counter_flow.dart';
+import 'package:provider/provider.dart';
+import 'presentation/notes_flow/notes_flow.dart';
+import 'presentation/notes_flow/home_page/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,18 +10,16 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (context) => NotesChanger(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'My Notes App',
+        theme: ThemeData(primarySwatch: Colors.pink),
+        home: const HomePage(),
       ),
-      home: CounterFlow(),
     );
   }
 }
-
